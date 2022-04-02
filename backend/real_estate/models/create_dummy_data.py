13f0057b4c1838433e9239ea0dfa191cfb9d477d
Nekoiii <10nekosa@gmail.com>
2022-04-02 11:10:11 +0900

    doing association_rule learning_models

--- backend/real_estate/models/create_dummy_data.py
+++ backend/real_estate/models/create_dummy_data.py
@@ -1,37 +1,50 @@
 # 用来创建虚拟数据
+import numpy as np
 import csv
 import random
 
-# 创建家具厂,记录1000条购买记录
+# 创建家具厂,记录100条购买记录
 furniture_list=[]
 def create_furniture_list():
     for i in range(65,91):#创建26个以大写字母为名的家具
         furniture_list.append(chr(i))
     print(furniture_list)
     return furniture_list
-#创建1000条记录, 把家具随机分配给每条记录
+#创建100条记录, 把家具随机分配给每条记录
 def distribute_funrniture(user_num,furniture_list):
     with open('furniture_factory.csv' , 'w') as f:
         f_writer = csv.writer(f)
-        funi_thro_list=[]#给每个家具随机生成一个阈值
+        
+        funi_thro_list=[]#给每个家具随机生成一个阈值        
         for i in furniture_list:
             threshold=random.randint(0,10)#随机生成阈值
             funi_thro_list.append(threshold)
                         
-        for i in range(0,user_num+1):#把家具随机分配给每条记录
+        max_cols=0
+        the_list=[]
+        for i in range(0,user_num):#把家具随机分配给每条记录
             a_list=[]
             for i_2,val_2 in  enumerate(furniture_list):
                 random_num=random.randint(0,10)#再随机生成一个数
                 if random_num>funi_thro_list[i_2]:#如果随机数>家具阈值,则放进去
                     a_list.append(furniture_list[i_2])
+            #a_list.extend((len(furniture_list)-len(a_list))\
+            #              *[''])
+            if len(a_list)>max_cols:
+                max_cols=len(a_list)
             print('a_list',a_list)
+            the_list.append(a_list)
+            
+        for a_list in the_list:#统一每行的列数,不然读取时会报错
+            if len(a_list) <max_cols:
+                a_list.extend((max_cols-len(a_list))*[None])
             f_writer.writerow(a_list)
     return
                 
 
 def create_furniture_factory():
     furniture_list=create_furniture_list()
-    distribute_funrniture(1000,furniture_list)
+    distribute_funrniture(100,furniture_list)
     
     return     
     
--- backend/real_estate/models/predictModels/association_rule learning_models.py
+++ backend/real_estate/models/predictModels/association_rule learning_models.py
@@ -7,8 +7,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.metrics import r2_score
-import plot_charts
 
-dataset_csv = ''
+dataset_csv='furniture_factory.csv'
+#dataset_csv='Market_Basket_Optimisation.csv'
 
+def do_association_rule_learning(model):
+    #* 注:这次csv没有表头所以记得加上header=None
+    df=pd.read_csv(dataset_csv,header=None)
+    total_rows=len(df.axes[0])#获取总行数
+    total_cols=len(df.axes[1])#获取总列数
+    #print(total_rows,total_cols)
+    
+    '''old_transactions = []
+    for i in range(0, 10):
+        old_transactions.append([str(df.values[i,j])for j in range(0, 18)])
+
+    print('a-1',old_transactions[0])
+    transactions=[]
+    for a_list in old_transactions:#去掉空值
+        new_list=list(filter(lambda x : x != 'nan', a_list))
+        transactions.append(new_list)
+    print('a-2',transactions[0])'''
+    
+    transactions = []
+    for i in range(0, 100):
+        transactions.append([str(df.values[i,j]) for j in range(0, 18)])
+
+    from apyori import apriori
+    rules = apriori(transactions = transactions,\
+                    min_support = 0.003, min_confidence = 0.2,\
+                    min_lift = 3, min_length = 2, \
+                    max_length = 2)
+        
+    #print(rules)
+    results = list(rules)
+    results
+    print(results)
+    
+    return
+
+do_association_rule_learning('Apriori')
