d3e6bc045f1f2245cebc24d666d44bae06aa49b9
Nekoiii <10nekosa@gmail.com>
2023-04-06 22:51:12 +0900

    was able to scroll but some bugs

--- music_processing/piano.py
+++ music_processing/piano.py
@@ -27,6 +27,7 @@ screen = pygame.display.set_mode((800,600))
 pygame.display.update() 
 scroll_position = 0  # 当前窗口应该显示图片的位置
 max_y=screen_h
+x,y=50,50; #绘制时的初始坐标
 
 
 def play_audio(path,key):
@@ -52,49 +53,52 @@ def play_audio(path,key):
         data = wf.readframes(CHUNK)
   
   
+def reload_img():
+  global max_y
+  image = pygame.image.load(png_path)
+  scaled_w=0.9*screen_w;
+  img_w=image.get_width();
+  img_h=image.get_height()
+  scale_factor=scaled_w/img_w #缩放图片
+  scaled_h=img_h * scale_factor
+  max_y=y+scaled_h  
+  return(image,scaled_w,scaled_h)
+  
 def re_draw():
-        # 清空窗口并绘制背景色(不然图片移动时不会恢复底色)
-        screen.fill((0, 0, 0))
-        x,y=50,50;
+  global x,y
+  # 清空窗口并绘制背景色(不然图片移动时不会恢复底色)
+  screen.fill((0, 0, 0))
 
-        # 计算图片缩放后大小
-        image = pygame.image.load(png_path)
-        scaled_w=0.9*screen_w;
-        img_w=image.get_width();
-        img_h=image.get_height()
-        scale_factor=scaled_w/img_w #缩放图片
-        scaled_h=img_h * scale_factor
-        max_y=y+scaled_h
-        # 在另一个更大的 Surface 上绘制, 以实现滚动效果
-        scroll_surface = pygame.Surface((screen_w, max_y))
+  image,scaled_w,scaled_h=reload_img()
+  # 在另一个更大的 Surface 上绘制, 以实现滚动效果
+  scroll_surface = pygame.Surface((screen_w, max_y))
 
-        #输出字
-        text_list.append(keys_map[key])
-        items_per_line = 10   # 设置每行显示的元素个数
-        # 将列表分成每10个元素一组，组内用逗号分隔pygame
-        #*注: pygame默认字体无法识别换行符\n,所以只能一行行计算坐标显示！
-        grouped_items = [text_list[i:i+items_per_line] for i in range(0, len(text_list), items_per_line)]
-        grouped_items = [', '.join(group) for group in grouped_items]
-        text_surfaces = []
-        for item in grouped_items:
-          text_surface=font.render(item, True, (255, 255, 255))
-          text_surfaces.append(text_surface)
-        for text_surface in text_surfaces:
-          scroll_surface.blit(text_surface, (x, y))
-          screen.blit(scroll_surface, (0,0))
-          y+=30
-        
-        #绘制图片
-        scroll_surface.blit(pygame.transform.scale
-                    (image, (scaled_w, scaled_h)), 
-                    (0.5*(screen_w-scaled_w), y))
-        #print('scroll_position--',scroll_position)
-        # 根据 scroll_position 将 Surface 上的一部分绘制到屏幕上
-        scroll_rect = pygame.Rect(0, scroll_position, screen_w, screen_h)
-        screen.blit(scroll_surface.subsurface(scroll_rect), (0, 0))
-        
-        
-        pygame.display.update()
+  #输出字
+  items_per_line = 10   # 设置每行显示的元素个数
+  # 将列表分成每10个元素一组，组内用逗号分隔pygame
+  #*注: pygame默认字体无法识别换行符\n,所以只能一行行计算坐标显示！
+  grouped_items = [text_list[i:i+items_per_line] for i in range(0, len(text_list), items_per_line)]
+  grouped_items = [', '.join(group) for group in grouped_items]
+  text_surfaces = []
+  for item in grouped_items:
+    text_surface=font.render(item, True, (255, 255, 255))
+    text_surfaces.append(text_surface)
+  for text_surface in text_surfaces:
+    scroll_surface.blit(text_surface, (x, y))
+    screen.blit(scroll_surface, (0,0))
+    y+=30
+  
+  #绘制图片
+  if len(text_list)>0:
+    scroll_surface.blit(pygame.transform.scale
+                (image, (scaled_w, scaled_h)), 
+                (0.5*(screen_w-scaled_w), y))
+    
+  # 根据 scroll_position 将 Surface 上的一部分绘制到屏幕上
+  scroll_rect = pygame.Rect(0, scroll_position, screen_w, screen_h)
+  screen.blit(scroll_surface.subsurface(scroll_rect), (0, 0))
+  
+  pygame.display.update()
 
 
 text_list=[] #显示的文字
@@ -109,16 +113,19 @@ while not if_quited:
       print('KEYDOWN--')
       
       if key ==pygame.K_UP:    #上下滚动窗口
-          print('---K_UP')
-          scroll_position -= 10
+          reload_img()
+          scroll_position -= 100
           if scroll_position < 0:
               scroll_position = 0
+          #print('---K_UP--scroll_position',scroll_position)
           re_draw()
       elif event.key ==pygame.K_DOWN:
-          print('---K_DOWN')
-          scroll_position += 10
-          if scroll_position > max_y - screen_h-1:
+          image,scaled_w,scaled_h=reload_img()
+          scroll_position += 100
+          print('---K_DOWN--scroll_position111',scroll_position,max_y - screen_h)
+          if scroll_position > (max_y - screen_h):
               scroll_position = max_y - screen_h
+          print('---K_DOWN--scroll_position222',scroll_position,max_y - screen_h)
           re_draw()
       elif(key == pygame.K_ESCAPE):    #esc键退出
         if_quited=True
@@ -132,6 +139,8 @@ while not if_quited:
         if os.path.exists(fileName):
           threading.Thread(target=play_audio, args=(fileName,key)).start()
           
+        #绘制
+        text_list.append(keys_map[key])
         re_draw()
 
       
