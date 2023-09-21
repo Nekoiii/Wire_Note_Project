ff4d8aee4c22d8950112409b4e68b79c9031fe30
Nekoiii <10nekosa@gmail.com>
2023-09-22 08:44:32 +0900

    restructure

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+
+BASE_PATH='/Users/a/code/Wire_Note_Project'
--- main_scripts/try_yolov8/config.py
+++ main_scripts/try_yolov8/config.py
@@ -1,2 +1,4 @@
+from config import BASE_PATH
+
 CLASSES = ['cable', 'tower_wooden']
 DATASET_BASE_PATH = '/Users/a/code/Wire_Note_Project/ttpla_dataset/data_original_size_v1'
